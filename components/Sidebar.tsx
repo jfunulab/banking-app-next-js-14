e28f1825383345e757dcc6b4042bbd20ac74f375
Joel Funu <jfunu79@gmail.com>
2024-05-06 06:10:12 +0000

    implement home dashboard WIP

---
+++ components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import {sidebarLinks} from "../constants/index";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+    return (
+        <section className="sidebar">
+            <nav className="flex flex-col gap-4">
+                <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+                    <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo" className="size-[24px] max-xl:size-14"/>
+                    <h1 className="sidebar-logo">Horizon</h1>
+                </Link>
+
+                {sidebarLinks.map((item) => {
+                    return (
+                        <Link href={item.route} key={item.label} className="">
+                            {item.label}
+                        </Link>
+                    )
+                })}
+            </nav>
+        </section>
+    )
+}
+
+export default Sidebar
